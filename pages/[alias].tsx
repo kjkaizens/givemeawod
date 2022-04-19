52195dc6db75445a37b07d6d6ad4e22a74db8481
kjacob <kevin.jacob@eficode.com>
2022-04-19 09:36:41 +0300

    remove vercel link from footer

--- pages/[alias].tsx
+++ pages/[alias].tsx
@@ -55,7 +55,6 @@ const PageLayout = (props:{children:React.ReactChild}) =>{
 
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
         >
