ac0e79238a5e9d0c59c766ce1eec51e160028096
kjacob <kevin.jacob@eficode.com>
2022-05-25 10:58:46 +0300

    Added Navbar and title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,12 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import Navbar from '../components/Navbar'
 
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+  <Navbar></Navbar>
+  <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
