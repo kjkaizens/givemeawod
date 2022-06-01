258b0e5ac7b9040d82999e98e851f8a38b462955
kjacob <kevin.jacob@eficode.com>
2022-06-01 13:30:54 +0300

    font thickness increased

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,7 @@ import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <>
-  <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'></link>
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600&display=swap" rel="stylesheet"></link>
   <Navbar></Navbar>
   <Component {...pageProps} />
   </>
